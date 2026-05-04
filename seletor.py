430e426abd9e2a6b6ca5a1b098ee3538e63b476a
rodriguesvicenzzo0-ship-it <rodriguesvicenzzo0@gmail.com>
2026-05-04 09:33:23 -0300

    v1

---
+++ seletor.py
@@ -0,0 +1,8 @@
+#--Seletor de caracteres--
+letra = input("digite a senha")
+if letra == "A":
+  print("Você escolheu a letra A")
+elif letra == "B":
+  print("Você escolheu a letra B")
+else:
+  print("Essa opção não está disponivel")
