993cd8989f3ce743fe491eea303b34428ab333e6
rodriguesvicenzzo0-ship-it <rodriguesvicenzzo0@gmail.com>
2026-05-04 14:09:25 +0000

    v2

--- seletor.py
+++ seletor.py
@@ -1,8 +1,9 @@
-#--Seletor de caracteres--
-letra = input("digite a senha")
+print("Sistema Seletor de Caracteres🔠")
+print("Escolha uma Letra e veja se ela está Disponivél no Sistema 🧟‍♀️")
+letra = input("digite a letra: ").upper()
 if letra == "A":
-  print("Você escolheu a letra A")
+  print("Você escolheu a letra A ✅")
 elif letra == "B":
-  print("Você escolheu a letra B")
+  print("Você escolheu a letra B 👍")
 else:
-  print("Essa opção não está disponivel")
+  print("Essa opção não está disponivel ❌")
--- var_sentinela.py
+++ var_sentinela.py
@@ -1,1 +1,6 @@
-
+#exemplo de uso da variavel sentinela
+while True:
+    comando = input("digite um comando-para para digite 'sair'")
+    if comando == "sair":
+      break
+print(f"executando:{comando}")
