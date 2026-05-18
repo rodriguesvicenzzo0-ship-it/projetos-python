ee859812e522d12e958441472c0c066f1a4ff019
rodriguesvicenzzo0-ship-it <rodriguesvicenzzo0@gmail.com>
2026-05-18 14:21:07 +0000

    v1

---
+++ tabuada.py
@@ -0,0 +1,5 @@
+numero = int(input("Tabuadaa de qual numero? "))
+print(f"--- Tabuada do {numero} ---")
+for kratos in range(1, 11):
+    resultado = numero * kratos
+    print(f"{numero} X {kratos} = {resultado}")
