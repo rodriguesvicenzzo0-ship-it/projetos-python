93eae73ac6421e52360ffc7bd0dc3395bc8af020
rodriguesvicenzzo0-ship-it <rodriguesvicenzzo0@gmail.com>
2026-05-25 12:53:47 +0000

    v1

---
+++ cabecalho.py
@@ -0,0 +1,10 @@
+### definindo e chamando aa função
+def exibir_cabecalho():
+    print('=' * 30)
+    print('SISTEMA DE GESTÃO ESCOLAR')
+    print('Versão 1.0 - Desenvolvimento no codespace')
+    print('=' * 30)
+
+    ### chamado a função
+    exibir_cabecalho()
+    print('Menu principal')
