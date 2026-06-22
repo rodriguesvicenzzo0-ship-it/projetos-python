1bd0ac98f15a5118be5e0e0a7f019dc055eb8c9a
rodriguesvicenzzo0-ship-it <rodriguesvicenzzo0@gmail.com>
2026-06-22 14:10:59 +0000

    v1

---
+++ podio.py
@@ -0,0 +1,4 @@
+vencedores = ["vicenzzo", "lorran", "davi"]
+print(f"medalha de ouro vai para: {vencedores[0]}")
+print(f"medalha de prata vai para: {vencedores[1]}")
+print(f"medalha de bronze vai para: {vencedores[2]}")
