77323a1ff91fd592a1aacb8345cbdac32efe8d14
rodriguesvicenzzo0-ship-it <rodriguesvicenzzo0@gmail.com>
2026-06-22 12:28:45 +0000

    v1

---
+++ painel.py
@@ -0,0 +1,6 @@
+print("--- PAINEL DE ATENDIMENTO ---")
+Fila_espera = ["senha 01","senha 02", "senha 03", "senha 04"]
+print(f"O próximo paciente a ser atendido é: {Fila_espera[0]}")
+print(f"O segundo paciente a ser atendido é: {Fila_espera[1]}")
+total = len(Fila_espera)
+print(f"Total de pacientes na fila: {total}")
